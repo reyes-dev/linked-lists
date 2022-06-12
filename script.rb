24f07a3e23452f52d08a6467b7c84bc720a59eae
Jarrod Reyes <jarrodreyes98@gmail.com>
2022-06-12 14:01:35 -0700

    Add node and linked list classes

---
+++ script.rb
@@ -0,0 +1,53 @@
+class LinkedList
+  attr_accessor :name
+
+  def initialize
+    @head = nil
+    @tail = nil
+  end
+
+  def append(value)
+  end
+
+  def prepend(value)
+  end
+
+  def size
+  end
+
+  def head
+  end
+
+  def tail
+  end
+
+  def at(index)
+  end
+
+  def pop
+  end
+
+  def contains?(value)
+  end
+
+  def find(value)
+  end
+
+  def to_s
+  end
+
+  def insert_at(value, index)
+  end
+
+  def remove_at(index)
+  end
+end
+
+class Node
+  attr_accessor :value, :next_node
+
+  def initialize(value = nil, next_node = nil)
+    @value = value
+    @next_node = next_node
+  end
+end
