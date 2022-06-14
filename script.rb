d5b6bf4ea5f79af85c6920ac7f97ffc595f31ddf
Jarrod Reyes <jarrodreyes98@gmail.com>
2022-06-13 21:41:33 -0700

    Add linkedlist append method functionality

--- script.rb
+++ script.rb
@@ -3,42 +3,59 @@ class LinkedList
 
   def initialize
     @head = nil
-    @tail = nil
   end
 
+  # Adds a new Node object containing a value to the END of the list
   def append(value)
+    if @head.nil?
+      @head = Node.new(value)
+    else
+      tail.next_node = Node.new(value)
+    end
   end
-
+  # Adds a new Node objecting containing a value to the START of the list
   def prepend(value)
+    # Assign a new node as the head and tail
+    #if @head.nil?
+      #@head = Node.new(value)
+      #@tail = @head
+    # If there's already a node object in head 
+    #else # Head pointer holds an entry
+      #@head.next =  
+    #end
   end
-
+  # Returns the total number of Node objects in the list
   def size
   end
-
+  # Returns the first Node object in the list
   def head
   end
-
+  # Returns the last Node object in the list
   def tail
-  end
+    node = @head
 
+    return node if !node.next_node
+    return node if !node.next_node while (node = node.next_node)
+  end
+  # Returns the Node object at the given INDEX
   def at(index)
   end
-
+  # Removes the last Node object from the list (the tail?)
   def pop
   end
-
+  # Returns true if the passed value is in the list, otherwise returns false
   def contains?(value)
   end
-
+  # Returns the INDEX of the Node object containing the passed value, or nil if not found
   def find(value)
   end
-
+  # Shows Node objects as strings and prints them out to preview to the console
   def to_s
   end
-
+  # Inserts a new Node objects with the given VALUE at the given INDEX
   def insert_at(value, index)
   end
-
+  # Removes the Node object at the given INDEX
   def remove_at(index)
   end
 end
@@ -51,3 +68,11 @@ class Node
     @next_node = next_node
   end
 end
+
+list = LinkedList.new
+list.append(47)
+list.append(22)
+list.append(6)
+list.append(3)
+
+p list
